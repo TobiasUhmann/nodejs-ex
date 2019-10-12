b030037edbd78c8eec4ed62224e7dde5d96526fa
Tobias Uhmann <tobiasuhmann@gmail.com>
2019-10-12 16:48:14 +0200

    cors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express = require('express'),
 
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const Track = require('./models/track.js')
     
@@ -15,6 +16,7 @@ app.use(morgan('combined'))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+server.use(cors())
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
